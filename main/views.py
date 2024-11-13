6016bf04a18957e727ca9916021aab1f107648e2
meisyanajla <meisyaanajlaa@gmail.com>
2024-11-13 13:59:14 +0700

    Finished Tutorial 8

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, reverse
+import json
+from django.shortcuts import render, redirect  
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
@@ -11,12 +12,14 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
+    
     context = {
         'name': request.user.username,
         'npm' : '2306209870',
@@ -68,22 +71,21 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
 
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-        else:
-            messages.error(request, "Invalid username or password. Please try again.")
-
-    else:
-        form = AuthenticationForm(request)
-    context = {'form': form}
-    return render(request, 'login.html', context)
+      if form.is_valid():
+           user = form.get_user()
+           login(request, user)
+           response = HttpResponseRedirect(reverse("main:show_main"))
+           response.set_cookie('last_login', str(datetime.datetime.now()))
+           return response
+      else:
+          messages.error(request, "Invalid username or password. Please try again.")
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
@@ -107,8 +109,11 @@ def edit_mood(request, id):
     return render(request, "edit_mood.html", context)
 
 def delete_mood(request, id):
+    # Get mood berdasarkan id
     mood = MoodEntry.objects.get(pk = id)
+    # Hapus mood
     mood.delete()
+    # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
 
 @csrf_exempt
@@ -127,3 +132,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
